9f8f8e58144e48376808e317e96271541160a5b5
Matheus Morcinek <matheusmorcinek@gmail.com>
2021-08-23 14:55:36 -0300

    weakmaps the begin

---
+++ maps-and-sets/weak-map-begin.js
@@ -0,0 +1,16 @@
+//WeakMap vs Map
+/*
+    The keys are weak, and can be garbage collected.
+
+    ***How they're the same***
+    
+    >  Both contain key-value pairs
+    >  Both allow you to set, get, delete and check for key-value pairs 
+
+    ***Key Differences***
+
+   >   The most significant difference is related to keys. With WeakMap's you cannot use primitive data type as a key, WeakMap's keys must be objects.
+   >   WeakMap does not have all of the same methods and property as Map, including the ability to iterate over the object or get its size. 
+
+*/
+
