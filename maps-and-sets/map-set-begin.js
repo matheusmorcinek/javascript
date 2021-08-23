8dbbe792ceb9b633d552287bc8392c75cdeecf6e
Matheus Morcinek <matheusmorcinek@gmail.com>
2021-08-23 10:40:59 -0300

    building the map with the set method

---
+++ maps-and-sets/map-set-begin.js
@@ -0,0 +1,16 @@
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
+
+//Saturday todo list
+// 8am Walk the dogs
+// 12pm Lunch
+// 3pm Watch a movie
+
+
+//Creating a new Map() and add todo as a key-value pair
+const saturday = new Map();
+
+saturday.set(8, 'walk the dogs');
+saturday.set(12, 'launch');
+saturday.set(3, 'watch a movie');
+
+console.log(saturday);
