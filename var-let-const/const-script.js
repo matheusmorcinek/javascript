83b899beaca0b2aeeff88b0f920c5802d5d89705
= <=>
2022-01-10 13:26:48 -0300

    const example

---
+++ var-let-const/const-script.js
@@ -0,0 +1,13 @@
+const pessoa = {
+    name: 'Matheus',
+    sobrenome: 'Morcinek'
+};
+
+console.log(pessoa);
+
+pessoa.name = 'Bruna';
+pessoa.sobrenome = 'Zacarias';
+
+console.log(pessoa);
+
+pessoa = {};
